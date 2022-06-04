70b74df8c2bedc416a18462d1d0985e6cac1058e
vymiz <vymiz020777@gmail.com>
2022-06-04 17:51:28 +0300

    field: __init__, draw, install base func

---
+++ field.py
@@ -0,0 +1,34 @@
+import random
+
+
+class Dish:
+
+    def __init__(self, raw, col):
+        self.raw = raw
+        self.col = col
+        self.field = []
+        for self.r in range(self.raw):
+            self.tmp = []
+            for self.c in range(self.col):
+                self.tmp.append(' ')
+            self.field.append(self.tmp)
+
+    def draw(self):
+        print('+' * (self.col+2))
+        for self.r in self.field:
+            print('+', end='')
+            for self.c in self.r:
+                print(self.c, end='')
+            print('+')
+        print('+' * (self.col + 2))
+
+    def install(self):
+        self.c = random.randint(0, self.col-1)
+        self.r = random.randint(0, self.raw-1)
+        self.field[self.r][self.c] = '#'
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+if __name__ == '__main__':
+    dish = Dish(5, 10)
+    dish.install()
+    dish.draw()
