2ca2689e83b9484158e81c1bc18b1df98d7f141c
VyMiZ <vymiz020777@gmail.com>
2022-06-09 06:14:23 +0300

    empty_check func finished, rand.choice added

--- cell.py
+++ cell.py
@@ -1,3 +1,4 @@
+import random
 class Cell:
 
     def __init__(self, raw, col):
@@ -10,7 +11,7 @@ class Cell:
         self.tmp_c = None
         self.tmp_r = None
         self.empty_arr = None
-
+        self.empty_field = None
 
     def __str__(self):
         return self.img
@@ -19,32 +20,27 @@ class Cell:
         field[self.raw][self.col] = ' '
 
     def empty_check(self, field):
-        self.empty_arr = []
-        for self.r in range(-1,2):
-            for self.c in range(-1,2):
-                if self.r != 0 or self.c != 0:
-                    self.tmp_r = self.raw + self.r
+        self.empty_arr = [] # empty tmp arr
+        for self.r in range(-1,2): # run through raws
+            for self.c in range(-1,2): # run through cols
+                if self.r != 0 or self.c != 0: # exclude zero variants
+                    self.tmp_r = self.raw + self.r # get coordinats of possibly empty field
                     self.tmp_c = self.col + self.c
-                if 0 <= self.tmp_r < len(field):
+                if 0 <= self.tmp_r < len(field): # check array's borders
                     if 0 <= self.tmp_c < len(field[0]):
-                        self.tmp_tuple = (self.tmp_r, self.tmp_c)
-                        if self.tmp_tuple not in self.empty_arr:
-                            self.empty_arr.append(self.tmp_tuple)
+                        self.tmp_tuple = (self.tmp_r, self.tmp_c) # assemble coordinates ia tuples
+                        if self.tmp_tuple not in self.empty_arr: # erase duplicates which get here from nothing
+                            if field[self.tmp_r][self.tmp_c] == ' ': # check is this field empty?
+                                self.empty_arr.append(self.tmp_tuple)
         print(self.empty_arr)
-
-
-    def test(self, field):
-        print(len(field))
-        print(len(field[0]))
-        print()
-
+        return random.choice(self.empty_arr) # returns self.empty_field
 
     def __call__(self, field):
-        # self.ctr += 1
-        # if self.ctr > 10:
-        #     self.apoptos(field)
-        # self.test(field)
-        self.empty_check(field)
+        self.ctr += 1
+        if self.ctr > 10:
+            self.apoptos(field)
+        self.empty_field = self.empty_check(field)
+        print(self.empty_field)
 
 
 if __name__ == '__main__':
